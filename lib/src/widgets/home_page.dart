df92ee894def573a52187ceefd652af86331140e
paul rublev <paulrublev@Mac.local>
2022-11-23 11:59:46 +0500

    Task 4, init

--- lib/src/widgets/home_page.dart
+++ lib/src/widgets/home_page.dart
@@ -1,8 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-
-import '../app_platform.dart';
-import 'page_content.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -13,132 +9,11 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String _textFieldValue = '';
-  final String _flutterDev = 'https://flutter.dev/';
-  late TextEditingController _editingController;
-
-  @override
-  void initState() {
-    _editingController = TextEditingController(text: _flutterDev);
-    super.initState();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[
-          _makePageView(),
-          _makeBottomBar(),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(10, 0, 10, 20),
-            child: Text(
-              "APPLICATION RUNNING ON "
-              "${AppPlatform.platform.toString().toUpperCase()}",
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  @override
-  void dispose() {
-    _editingController.dispose();
-    super.dispose();
-  }
-
-  Future<String> _loadingPage(String? name) async {
-    name = name ?? '';
-    if (name == '') return '';
-    Uri uriName = Uri.parse(name);
-    try {
-      final result = await http.get(uriName);
-      return result.body;
-    } catch (e) {
-      return '';
-    }
-  }
-
-  Widget _makePageView() {
-    return Expanded(
-      child: _textFieldValue == ''
-          ? Container()
-          : FutureBuilder<String>(
-              future: _loadingPage(_textFieldValue),
-              builder: ((context, snapshot) {
-                switch (snapshot.connectionState) {
-                  case ConnectionState.none:
-                    return Container();
-                  case ConnectionState.waiting:
-                    return const Padding(
-                      padding: EdgeInsets.only(
-                        top: 48.0,
-                      ),
-                      child: Center(
-                        child: CircularProgressIndicator(),
-                      ),
-                    );
-                  case ConnectionState.done:
-                    if (snapshot.hasError) {
-                      return Text('${snapshot.error}');
-                    } else if (snapshot.hasData) {
-                      return PageContent(
-                        content: snapshot.data,
-                        url: _textFieldValue,
-                      );
-                    }
-                    return Container();
-                  default:
-                    return Container();
-                }
-              }),
-            ),
-    );
-  }
-
-  Widget _makeBottomBar() {
-    return Container(
-      height: 90,
-      decoration: const BoxDecoration(
-        border: Border(
-          top: BorderSide(
-            color: Colors.black,
-            width: 2,
-          ),
-        ),
-      ),
-      padding: const EdgeInsets.all(20),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          SizedBox(
-            width: MediaQuery.of(context).size.width - 200,
-            child: TextField(
-              controller: _editingController,
-              style: const TextStyle(fontWeight: FontWeight.w800),
-              decoration: const InputDecoration(
-                enabledBorder: OutlineInputBorder(),
-                focusedBorder: OutlineInputBorder(),
-              ),
-            ),
-          ),
-          SizedBox(
-            width: 150,
-            child: ElevatedButton(
-              onPressed: (() {
-                _textFieldValue = _editingController.value.text;
-                setState(() {});
-              }),
-              child: const Text(
-                'LOAD',
-                textScaleFactor: 2,
-              ),
-            ),
-          ),
-        ],
+        children: <Widget>[],
       ),
     );
   }
